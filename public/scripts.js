0758e2f1e6e4049071260732c5df444aaa9e9a2a
Grzegorz Gajda <g.gajda1976@gmail.com>
2022-07-18 15:59:30 +0200

    post server added

--- public/scripts.js
+++ public/scripts.js
@@ -2,11 +2,34 @@ var convert = () => {
     const element = document.getElementById('toPdf');
     const opt = {
         margin: 1,
-        filename: 'PDFfile.pdf',
+        filename: 'KP.pdf',
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: { scale: 2 },
         jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
     };
 
-    html2pdf().set(opt).from(element).save();
+    //html2pdf().set(opt).from(element).save();
+
+    html2pdf().set(opt).from(element).toPdf().output('datauristring')
+    .then(function(pdfBase64) {
+        const file = new File(
+            [pdfBase64],
+            opt.filename,
+            // {type: 'application/pdf'}
+        ); 
+
+        const formData = new FormData();        
+        formData.append("KP.pdf", file);
+
+        fetch('http://localhost:3000/profile', {
+          method: 'POST',
+          body: formData,
+        })
+        .then(result => {
+          console.log('Success:', result);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    });
 }
--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express'),
+    multer = require('multer'),
     app = express(),
     port = 3000,
     server_message = `HTTP server is worked on port ${port}`;
+
+const storage = multer.diskStorage(
+    {
+        destination: './uploads/',
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        }
+    }
+),
+    upload = multer({storage});
 
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public/'));
 
 app.get('/status', (req, res) => {
     res.send(server_message)
+})
+
+app.post('/profile', upload.single('KP.pdf'), (req, res) => {
+    const file = req.file;
+
+    console.log(file)
+
+    res.send('OK');
 })
 
 app.listen(port, () => {
